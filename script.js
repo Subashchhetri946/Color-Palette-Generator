472c055d9d5c6a4554dc9248ab743e882091f500
Subash Chhetri <156183094+Subashchhetri946@users.noreply.github.com>
2026-07-09 22:46:17 +0300

    working with copy btn and also with the click

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 const generateBtn = document.getElementById("generate-btn");
 const paletteContainer = document.querySelector(".palette-container");
 
-generateBtn.addEventListener("click", generatePaltte)
+generateBtn.addEventListener("click", generatePalette);
 
-function generatePaltte() {
+paletteContainer.addEventListener("click", function (e) {
+    if(e.target.classList.contains("copy-btn")) {
+        const hexValue = e.target.previousElementSibling.textContent;
+
+        navigator.clipboard.writeText(hexValue)
+        .then(() => showCopySuccess(e.target))
+        .catch((err) => console.log(err));
+    } else if (e.target.classList.contains("color")) {
+        const hexValue = e.target.nextElementSibling.querySelector(".hex-value").textContent;
+        navigator.clipboard.writeText(hexValue)
+        .then(() => showCopySuccess(e.target.nextElementSibling.querySelector(".copy-btn")))
+        .catch((err) => console.log(err));
+    }
+});
+
+function showCopySuccess(element) {
+    element.classList.remove("far", "fa-copy");
+    element.classList.add("fas", "fa-check");
+
+    element.style.color = "#48bb78";
+
+    setTimeout(() => {
+        element.classList.remove("fas", "fa-check")
+        element.classList.add("far", "fa-copy")
+        element.style.color = ""
+    }, 1500)
+}
+
+function generatePalette() {
     const colors = []
     
     for(let i=0; i<5; i++) {
@@ -22,10 +50,10 @@ function generateRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color
-}
+} 
 
 
-function updatePaletteDisplay() {
+function updatePaletteDisplay(colors) {
     const colorBoxes = document.querySelectorAll(".color-box");
 
     colorBoxes.forEach((box, index) => {
